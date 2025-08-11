ca64e8449bea6173318a195b41e5f42eadc638f5
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2025-08-10 23:41:57 -0500

    feat: ocr func

--- models/main.py
+++ models/main.py
@@ -14,21 +14,7 @@ from langgraph.graph import START, MessagesState, StateGraph
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 from src.config import ContextoGeneral, PromptExtraccionPliegos
-
-# -------------------
-# Configuración Redis
-# -------------------
-REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "devpass123")
-
-# Redis client (async mode)
-redis_client = redis.Redis(
-    host=REDIS_HOST,
-    port=REDIS_PORT,
-    password=REDIS_PASSWORD,
-    decode_responses=False 
-)
+from src.redis_db import redis_client
 
 # -------------------
 # Paths
